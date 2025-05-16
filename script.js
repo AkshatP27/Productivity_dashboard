50eca6559947115c8d08037c6caa6bb75d25987a
Akshat Panchal <akshat.coder.04@gmail.com>
2025-05-16 23:38:56 +0530

    Implemented card click display functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// let allElems = document.querySelectorAll('.elem');
-// let allFullElems = document.querySelectorAll('.full-elem');
+let allElems = document.querySelectorAll('.elem');
+let allFullElems = document.querySelectorAll('.full-elem');
 
-// allElems.forEach((elem) => {
-//     // console.log(elem);
-//     elem.addEventListener('click', () => {
-//         // console.log(elem.id);
-//         // console.log(allFullElems[elem.id]);
-//         allFullElems[elem.id].style.display = 'block';
-//     })
-// })
+allElems.forEach((elem) => {
+    // console.log(elem);
+    elem.addEventListener('click', () => {
+        // console.log(elem.id);
+        // console.log(allFullElems[elem.id]);
+        allFullElems[elem.id].style.display = 'block';
+    })
+})
