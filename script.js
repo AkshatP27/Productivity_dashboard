387d513db0e96a737a3364149890791389ae27f5
Akshat Panchal <akshatp272004@gmail.com>
2025-05-16 23:35:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let allElems = document.querySelectorAll('.elem');
+// let allFullElems = document.querySelectorAll('.full-elem');
+
+// allElems.forEach((elem) => {
+//     // console.log(elem);
+//     elem.addEventListener('click', () => {
+//         // console.log(elem.id);
+//         // console.log(allFullElems[elem.id]);
+//         allFullElems[elem.id].style.display = 'block';
+//     })
+// })
