d9d1c1fbbf8a4936a03266382b1b1875b666a6fe
Akshat Panchal <akshat.coder.04@gmail.com>
2025-05-19 21:25:48 +0530

    Added back button functionality to individual full page of each card

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let allElems = document.querySelectorAll('.elem');
-let allFullElems = document.querySelectorAll('.full-elem');
+let fullElemsPage = document.querySelectorAll('.full-elem');
+let fullElemsPageBackBtn = document.querySelectorAll('.full-elem .back');
 
 allElems.forEach((elem) => {
     // console.log(elem);
     elem.addEventListener('click', () => {
         // console.log(elem.id);
         // console.log(allFullElems[elem.id]);
-        allFullElems[elem.id].style.display = 'block';
+        fullElemsPage[elem.id].style.display = 'block';
     })
 })
+
+fullElemsPageBackBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        // console.log(btn.id);
+        fullElemsPage[btn.id].style.display = 'none';
+    })
+    
+});
