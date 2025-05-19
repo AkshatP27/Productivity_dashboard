37312b026a613b3af5b1f4850a64778726e3a228
Akshat Panchal <akshat.coder.04@gmail.com>
2025-05-19 22:10:53 +0530

    Completed the UI of to-do list

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let allElems = document.querySelectorAll('.elem');
-let fullElemsPage = document.querySelectorAll('.full-elem');
-let fullElemsPageBackBtn = document.querySelectorAll('.full-elem .back');
+function openCard() {
+    let allElems = document.querySelectorAll('.elem');
+    let fullElemsPage = document.querySelectorAll('.full-elem');
+    let fullElemsPageBackBtn = document.querySelectorAll('.full-elem .back');
 
-allElems.forEach((elem) => {
-    // console.log(elem);
-    elem.addEventListener('click', () => {
-        // console.log(elem.id);
-        // console.log(allFullElems[elem.id]);
-        fullElemsPage[elem.id].style.display = 'block';
+    allElems.forEach((elem) => {
+        // console.log(elem);
+        elem.addEventListener('click', () => {
+            // console.log(elem.id);
+            // console.log(allFullElems[elem.id]);
+            fullElemsPage[elem.id].style.display = 'block';
+        })
     })
-})
 
-fullElemsPageBackBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        // console.log(btn.id);
-        fullElemsPage[btn.id].style.display = 'none';
-    })
-    
-});
+    fullElemsPageBackBtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            // console.log(btn.id);
+            fullElemsPage[btn.id].style.display = 'none';
+        })
+        
+    });
+}
+
+openCard();
+
